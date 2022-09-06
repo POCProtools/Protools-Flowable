4c1e1a3546e526ab91173c50dc579a776224b454
Mailine Nguyen <nguyen.mailine@gmail.com>
2022-09-06 10:45:21 +0200

    test conf again

--- src/main/java/com/protools/flowableDemo/configuration/RabbitMqConfig.java
+++ src/main/java/com/protools/flowableDemo/configuration/RabbitMqConfig.java
@@ -1,4 +1,4 @@
-package com.example.eventregistrypoc.config;
+package com.protools.flowableDemo.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.LogManager;
@@ -15,7 +15,8 @@ import org.springframework.amqp.rabbit.support.ListenerExecutionFailedException;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ErrorHandler;
 
 @EnableRabbit
