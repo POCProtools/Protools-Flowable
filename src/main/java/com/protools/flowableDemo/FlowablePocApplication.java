2d87d41ce84f17169e7c68ee7b5695f4be3bc163
Mailine Nguyen <nguyen.mailine@gmail.com>
2022-09-08 14:17:38 +0200

    test ?

--- src/main/java/com/protools/flowableDemo/FlowablePocApplication.java
+++ src/main/java/com/protools/flowableDemo/FlowablePocApplication.java
@@ -1,6 +1,5 @@
 package com.protools.flowableDemo;
 
-import com.protools.flowableDemo.services.WorkflowService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.flowable.engine.RepositoryService;
@@ -14,11 +13,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FlowablePocApplication {
 	private Logger logger = LogManager.getLogger(FlowablePocApplication.class);
 	private final RepositoryService repositoryService;
-	private final EventRepositoryService eventRepositoryService;
 
 	public FlowablePocApplication(RepositoryService repositoryService, EventRepositoryService eventRepositoryService) {
 		this.repositoryService = repositoryService;
-		this.eventRepositoryService = eventRepositoryService;
 	}
 
 	public static void main(String[] args) {
@@ -31,7 +28,7 @@ public class FlowablePocApplication {
 		logger.info("Application started.");
 
 		logger.info("Processes deployed: {}", repositoryService.createDeploymentQuery().count());
-		logger.info("Events deployed:    {}", eventRepositoryService.createDeploymentQuery().count());
+
 
 	}
 
